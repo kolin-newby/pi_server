5cdf347f84756440465057f29188dcfc9863fbd8
kolin-newby <kone9079@colorado.edu>
2019-11-10 23:00:52 +0000

    Add

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express'); // Add the express framework
-let app = express();
+const app = express();
 
 const bodyParser = require('body-parser'); // Add the body-parser tool
 app.use(bodyParser.json());              // Add support for JSON encoded bodies
@@ -14,13 +14,13 @@ const pgp = require('pg-promise')();
 //db config
 const dbConfig = {
 	host: '192.168.50.130',
-	port: 2048,
+	port: 1000,
 	database: 'traffic_db',
 	user: 'admin',
 	password: 'Appl3'
 };
 
-let db = pqp(dbConfig);
+let db = pgp(dbConfig);
 
 
 //sets pug as view engine
@@ -31,15 +31,17 @@ app.use(express.static(__dirname + '/'));
 
 
 
+app.get('/', function(req, res) {
+	res.render('pages/home',{
+	});
+});
 
 
 
 
 
-
-
-
-var port = 2048;
-app.listen(port);
+const hostname = '192.168.50.130';
+const port = 2048;
+app.listen(port, hostname);
 console.log('listening on port ' + port);
 
