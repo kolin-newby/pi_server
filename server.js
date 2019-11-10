1cf42ba19de766a482f40a058592e029516b8b15
sasl6573 <sasl6573@colorado.edu>
2019-11-10 15:56:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// server.js
+
 const express = require('express'); // Add the express framework
 let app = express();
 
@@ -14,7 +16,7 @@ const pgp = require('pg-promise')();
 //db config
 const dbConfig = {
 	host: '192.168.50.130',
-	port: 2048,
+	port: 5432,
 	database: 'traffic_db',
 	user: 'admin',
 	password: 'Appl3'
@@ -27,17 +29,31 @@ let db = pqp(dbConfig);
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/'));
 
+// home page
+app.get('/home', function(req, res) {
+	var query = ''; // select all locations and their latest reading
+	db.any(query)
+		.then(function (location_status) {
+			res.render('pages/home', {
+				page_title: 'Home',
+				data: location_status
+			})
+		})
+		.catch(function (err) {
+			console.log('error '+ err);
+			res.render('pages/home', {
+				page_title: 'Home',
+				data: '',
+			})
+		})
+});
 
-
-
-
-
-
-
-
-
-
-
+// location page 
+app.get('/location', function(req, res) {
+	res.render('pages/location',{
+		page_title:"Location Name"
+	});
+});
 
 var port = 2048;
 app.listen(port);
