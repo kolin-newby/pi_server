b1ed46764a3eed16cdbade4d765c42582f7a5f1a
sasl6573 <sasl6573@colorado.edu>
2019-11-30 15:28:47 -0700

    Home page should display locations and their current status

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,29 @@
 // script.js
+
+const QUIET_CUTOFF = ;
+const BUSY_CUTOFF = ;
+
+function getStatus (volume) {
+    
+    var status_class = 'color: black;';
+    var status_message = 'No Data';
+
+    // if closed()
+
+    if (volume) {
+        if (volume < QUIET_CUTOFF) {
+            status_class = 'color: green;';
+            status_message = 'Quiet';
+        }
+        else if (volume > BUSY_CUTOFF) {
+            status_class = 'color: red;';
+            status_message = 'Busy';
+        }
+        else {
+            status_class = 'color: yellow;';
+            status_message = 'Normal';
+        }
+    }
+
+    return {class: status_class, message: status_message};
+}
