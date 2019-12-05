913c3419000ce37572485a5c8e4d884334b42707
sasl6573 <sasl6573@colorado.edu>
2019-12-04 19:18:51 -0700

    test

--- server.js
+++ server.js
@@ -29,25 +29,63 @@ app.use(express.static(__dirname + '/'));
 
 //declare constants
 const REFRESH_INTERVAL = "120 minutes";
+const QUIET_CUTOFF = 100;
+const BUSY_CUTOFF = 200;
 
 // home page
 app.get('/', function(req, res) {
+	
 	// select all locations and their latest reading
 	var query = "SELECT DISTINCT ON (l.loc_id) l.loc_id AS id, l.loc_desc AS name, ";
 	query += "l.loc_hours AS hours, d.volume_db AS volume, d.time AS time ";
 	query += "FROM locations l LEFT JOIN data d ON d.loc_id = l.loc_id ";
 	query += "ORDER BY l.loc_id, d.time DESC;";
+	
 	console.log(query);
+	
 	db.any(query)
 		.then(function (location_status) {
 			console.log(location_status);
+			
+			var statuses = {};
+
+			for (var location in location_status) {
+
+				var status_class = 'color: black;';
+				var status_message = 'No Data';
+
+				// if closed()
+
+				if (location.volume) {
+					if (location.volume < QUIET_CUTOFF) {
+						status_class = 'color: blue;';
+						status_message = 'Quiet';
+					}
+					else if (location.volume > BUSY_CUTOFF) {
+						status_class = 'color: red;';
+						status_message = 'Busy';
+					}
+					else {
+						status_class = 'color: purple;';
+						status_message = 'Normal';
+					}
+				}
+
+				console.log("Class: "+status_class);
+				console.log("Message: "+status_message);
+
+				status[location.id] = [status_class, status_message];
+			}
+
 			res.render('pages/home', {
 				page_title: 'Home',
-				data: location_status
+				data: location_status,
+				status: statuses
 			})
 		})
 		.catch(function (err) {
 			console.log(err);
+
 			res.render('pages/home', {
 				page_title: 'Home',
 				data: ''
