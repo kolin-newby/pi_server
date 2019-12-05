aa68690ac9ea03dd90c5a4868af962f0d6d5960c
sasl6573 <sasl6573@colorado.edu>
2019-12-05 01:25:06 -0700

    test

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,1 @@
 // script.js
-
-var display_data = [];
-for (var index = 0; index < entry_count; index++) {
-    var entry = {
-        volume: '#{display_data[index].volume}',
-        time: '#{display_data[index].time}'
-    };
-    display_data.push(entry);
-}
